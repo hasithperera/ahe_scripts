98d008b110fd28af0fb6ef91dc9d78935a6a86d0
Hasith Perera <12579141+hasithperera@users.noreply.github.com>
2022-04-05 19:38:49 -0400

    Added Thinkpad specific statements

--- ahe_init.sh
+++ ahe_init.sh
@@ -7,12 +7,29 @@ chmod 755 ~/.bash_aliases
 
 #custom key binds for tab switching
 dconf load / < ./key_binds.conf
+mkdir ~/in
+mkdir ~/Projects
 
 
 # add battery logging script to crontab
 
 loc=$(pwd)
-(crontab -u $(whoami) -l; echo "@reboot $loc/batlog.sh #ahe" ) | crontab -u $(whoami) -
+(crontab - u $(whoami) -l; echo "@reboot $loc/batlog.sh #ahe" ) | crontab -u $(whoami) -
+
+
+#Add customizations for thinkpads
+
+lap_model=$(sudo lshw | grep "ThinkPad"|wc -l)
+
+if [[ $lap_model -gt 0 ]];
+then
+	echo "Add think pad battery controls"
+	echo 40 | sudo tee /sys/class/power_supply/BAT1/charge_start_threshold
+	echo 80 | sudo tee /sys/class/power_supply/BAT1/charge_start_threshold
+else
+	echo "Not a Thinkpad"
+fi
+
 
 sudo mkdir /opt/data_log/
 sudo chown $(whoami) /opt/data_log
@@ -20,8 +37,6 @@ sudo chown $(whoami) /opt/data_log
 black='\e[0;30m'
 white='\e[0;37m'
 yellow='\e[0;33m'
-
-
 echo -e "${yellow} "; crontab -l
 echo -e "${white}"
 echo "End of script"
