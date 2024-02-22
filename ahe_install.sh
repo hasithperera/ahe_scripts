2c5edc68a0c1fc57426317b55891221b53bae44b
Hasith Perera <12579141+hasithperera@users.noreply.github.com>
2024-02-22 08:57:35 -0500

    added package installation

---
+++ ahe_install.sh
@@ -0,0 +1,14 @@
+
+#obsidian
+
+#cd /tmp
+#wget https://github.com/obsidianmd/obsidian-releases/releases/download/v1.5.3/Obsidian-1.5.3.AppImage
+
+
+## MUC
+
+sudo apt install gcc-avr avrdude avr-libc binutils-avr -y
+
+# Serial 
+sudo apt install picocom
+xargs sudo apt-get -y install < app_list
